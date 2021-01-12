54de0482e4dfe33872d106578927461d471c84c6
linux_china <libing.chen@gmail.com>
2021-01-12 13:15:18 -0800

    QUIC Constants

---
+++ src/main/java/io/rsocket/transport/quic/QuicConstants.java
@@ -0,0 +1,10 @@
+package io.rsocket.transport.quic;
+
+/**
+ * QUIC Constants
+ *
+ * @author linux_china
+ */
+public interface QuicConstants {
+    byte[] proto = new byte[]{0x08, 'r', 's', 'o', 'c', 'k', 'e', 't', '/', '1', '.', '0'};
+}
