1e10d629adadb2ba80f407834cd36194a16ba8ac
linux_china <libing.chen@gmail.com>
2021-10-15 22:58:49 -0700

    Polishing

--- src/main/java/io/rsocket/transport/quic/client/QuicClientTransport.java
+++ src/main/java/io/rsocket/transport/quic/client/QuicClientTransport.java
@@ -5,7 +5,6 @@ import io.netty.incubator.codec.quic.QuicSslContext;
 import io.netty.incubator.codec.quic.QuicSslContextBuilder;
 import io.rsocket.DuplexConnection;
 import io.rsocket.transport.ClientTransport;
-import io.rsocket.transport.quic.QuicDuplexConnection;
 import io.rsocket.transport.quic.RSocketLengthCodec;
 import reactor.core.publisher.Mono;
 import reactor.netty.incubator.quic.QuicClient;
@@ -54,6 +53,6 @@ public class QuicClientTransport implements ClientTransport {
                 .initialSettings(spec -> spec.maxData(10000000).maxStreamDataBidirectionalLocal(1000000))
                 .doOnConnected(c -> c.addHandlerLast(new RSocketLengthCodec(maxFrameLength)))
                 .connect()
-                .map(QuicDuplexConnection::new);
+                .map(QuicDuplexClientConnection::new);
     }
 }
