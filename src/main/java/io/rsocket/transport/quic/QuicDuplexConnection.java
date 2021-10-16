2a483db12fe24a4923b1d43104d6f33e9e3dc531
linux_china <libing.chen@gmail.com>
2021-10-15 18:26:22 -0700

    Polishing

--- src/main/java/io/rsocket/transport/quic/QuicDuplexConnection.java
+++ src/main/java/io/rsocket/transport/quic/QuicDuplexConnection.java
@@ -2,51 +2,86 @@ package io.rsocket.transport.quic;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
-import io.rsocket.DuplexConnection;
 import io.rsocket.RSocketErrorException;
+import io.rsocket.frame.ErrorFrameCodec;
+import io.rsocket.frame.FrameLengthCodec;
+import io.rsocket.internal.BaseDuplexConnection;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import reactor.netty.Connection;
 
 import java.net.SocketAddress;
+import java.util.Objects;
 
 /**
  * QUIC duplex connection
  *
  * @author linux_china
  */
-public class QuicDuplexConnection implements DuplexConnection {
-    @Override
-    public void sendFrame(int streamId, ByteBuf frame) {
+public class QuicDuplexConnection extends BaseDuplexConnection {
+    private final Connection connection;
 
-    }
+    /**
+     * Creates a new instance
+     *
+     * @param connection the {@link Connection} for managing the server
+     */
+    public QuicDuplexConnection(Connection connection) {
+        super();
+        this.connection = Objects.requireNonNull(connection, "connection must not be null");
+        connection
+                .channel()
+                .closeFuture()
+                .addListener(
+                        future -> {
+                            if (!isDisposed()) dispose();
+                        });
 
-    @Override
-    public void sendErrorAndClose(RSocketErrorException errorException) {
-
-    }
-
-    @Override
-    public Flux<ByteBuf> receive() {
-        return null;
+        connection.outbound().send(sender).then().subscribe();
     }
 
     @Override
     public ByteBufAllocator alloc() {
-        return null;
+        return connection.channel().alloc();
     }
 
     @Override
     public SocketAddress remoteAddress() {
-        return null;
+        return connection.channel().remoteAddress();
     }
 
     @Override
-    public Mono<Void> onClose() {
-        return null;
+    protected void doOnClose() {
+        sender.dispose();
+        connection.dispose();
     }
 
     @Override
-    public void dispose() {
+    public void sendErrorAndClose(RSocketErrorException e) {
+        final ByteBuf errorFrame = ErrorFrameCodec.encode(alloc(), 0, e);
+        connection
+                .outbound()
+                .sendObject(FrameLengthCodec.encode(alloc(), errorFrame.readableBytes(), errorFrame))
+                .then()
+                .subscribe(
+                        null,
+                        t -> onClose.tryEmitError(t),
+                        () -> {
+                            final Throwable cause = e.getCause();
+                            if (cause == null) {
+                                onClose.tryEmitEmpty();
+                            } else {
+                                onClose.tryEmitError(cause);
+                            }
+                        });
+    }
 
+    @Override
+    public Flux<ByteBuf> receive() {
+        return connection.inbound().receive().map(FrameLengthCodec::frame);
+    }
+
+    @Override
+    public void sendFrame(int streamId, ByteBuf frame) {
+        super.sendFrame(streamId, FrameLengthCodec.encode(alloc(), frame.readableBytes(), frame));
     }
 }
