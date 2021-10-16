a5866bbb9c66ac02e78129d1107c1860627d5d9d
linux_china <libing.chen@gmail.com>
2021-10-16 11:27:28 -0700

    Add createClient()

--- src/main/java/io/rsocket/transport/quic/client/QuicClientTransport.java
+++ src/main/java/io/rsocket/transport/quic/client/QuicClientTransport.java
@@ -11,10 +11,13 @@ import reactor.core.publisher.Mono;
 import reactor.netty.incubator.quic.QuicClient;
 
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.time.Duration;
 import java.util.Objects;
+import java.util.function.Supplier;
 
 import static io.rsocket.frame.FrameLengthCodec.FRAME_LENGTH_MASK;
+import static reactor.netty.ConnectionObserver.State.CONNECTED;
 
 /**
  * QUIC client transport
@@ -31,7 +34,7 @@ public class QuicClientTransport implements ClientTransport {
 
     public static QuicClientTransport create(String bindAddress, int port) {
         Objects.requireNonNull(bindAddress, "bindAddress must not be null");
-        QuicClient quicClient = QuicClient.create().remoteAddress(() -> new InetSocketAddress(bindAddress, port));
+        QuicClient quicClient = createClient(() -> new InetSocketAddress(bindAddress, port));
         return create(quicClient);
     }
 
@@ -42,18 +45,34 @@ public class QuicClientTransport implements ClientTransport {
 
     @Override
     public Mono<DuplexConnection> connect() {
+        return client.streamObserve((conn, state) -> {
+                    System.out.println("stream observer");
+                    if (state == CONNECTED) {
+                        conn.addHandlerLast(new RSocketLengthCodec(maxFrameLength));
+                    }
+                })
+                .connect()
+                .map(QuicDuplexConnection::new);
+    }
+
+    static QuicClient createClient(Supplier<SocketAddress> remoteAddress) {
         QuicSslContext clientCtx = QuicSslContextBuilder.forClient()
                 .trustManager(InsecureTrustManagerFactory.INSTANCE)
                 .applicationProtocols("rsocket/1.0")
                 .build();
-        return client
+        return QuicClient.create()
+                .remoteAddress(remoteAddress)
                 .bindAddress(() -> new InetSocketAddress(0))
-                .secure(clientCtx)
                 .wiretap(false)
+                .secure(clientCtx)
                 .idleTimeout(Duration.ofSeconds(5))
-                .initialSettings(spec -> spec.maxData(10000000).maxStreamDataBidirectionalLocal(1000000))
-                .doOnConnected(c -> c.addHandlerLast(new RSocketLengthCodec(maxFrameLength)))
-                .connect()
-                .map(QuicDuplexConnection::new);
+                .initialSettings(spec -> {
+                    spec.maxData(10000000)
+                            .maxStreamDataBidirectionalLocal(1000000)
+                            .maxStreamDataBidirectionalRemote(1000000)
+                            .maxStreamDataUnidirectional(1000000)
+                            .maxStreamsBidirectional(100)
+                            .maxStreamsUnidirectional(100);
+                });
     }
 }
