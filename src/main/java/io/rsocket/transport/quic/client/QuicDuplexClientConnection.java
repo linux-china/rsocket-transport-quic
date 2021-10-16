6d92038da64d254fa758883438c680f6da445440
linux_china <libing.chen@gmail.com>
2021-10-15 23:01:23 -0700

    QuicDuplexClientConnection

--- src/main/java/io/rsocket/transport/quic/client/QuicDuplexClientConnection.java
+++ src/main/java/io/rsocket/transport/quic/client/QuicDuplexClientConnection.java
@@ -1,42 +1,41 @@
-package io.rsocket.transport.quic;
+package io.rsocket.transport.quic.client;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufAllocator;
+import io.rsocket.DuplexConnection;
 import io.rsocket.RSocketErrorException;
 import io.rsocket.frame.ErrorFrameCodec;
 import io.rsocket.frame.FrameLengthCodec;
-import io.rsocket.internal.BaseDuplexConnection;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
 import reactor.netty.Connection;
+import reactor.netty.NettyInbound;
 
 import java.net.SocketAddress;
-import java.util.Objects;
 
 /**
  * QUIC duplex connection
  *
  * @author linux_china
  */
-public class QuicDuplexConnection extends BaseDuplexConnection {
+public class QuicDuplexClientConnection implements DuplexConnection {
+    protected Sinks.Empty<Void> onClose = Sinks.empty();
     private final Connection connection;
 
     /**
      * Creates a new instance
-     *
-     * @param connection the {@link Connection} for managing the server
      */
-    public QuicDuplexConnection(Connection connection) {
+    public QuicDuplexClientConnection(Connection connection) {
         super();
-        this.connection = Objects.requireNonNull(connection, "connection must not be null");
-        connection
+        this.connection = connection;
+        this.connection
                 .channel()
                 .closeFuture()
                 .addListener(
                         future -> {
                             if (!isDisposed()) dispose();
                         });
-
-        connection.outbound().send(sender).then().subscribe();
     }
 
     @Override
@@ -49,17 +48,11 @@ public class QuicDuplexConnection extends BaseDuplexConnection {
         return connection.channel().remoteAddress();
     }
 
-    @Override
-    protected void doOnClose() {
-        sender.dispose();
-        connection.dispose();
-    }
 
     @Override
     public void sendErrorAndClose(RSocketErrorException e) {
         final ByteBuf errorFrame = ErrorFrameCodec.encode(alloc(), 0, e);
-        connection
-                .outbound()
+        connection.outbound()
                 .sendObject(FrameLengthCodec.encode(alloc(), errorFrame.readableBytes(), errorFrame))
                 .then()
                 .subscribe(
@@ -77,11 +70,23 @@ public class QuicDuplexConnection extends BaseDuplexConnection {
 
     @Override
     public Flux<ByteBuf> receive() {
-        return connection.inbound().receive().map(FrameLengthCodec::frame);
+        final NettyInbound inbound = connection.inbound();
+        return inbound.receive().map(FrameLengthCodec::frame);
     }
 
     @Override
     public void sendFrame(int streamId, ByteBuf frame) {
-        super.sendFrame(streamId, FrameLengthCodec.encode(alloc(), frame.readableBytes(), frame));
+        connection.outbound().send(Mono.just(FrameLengthCodec.encode(alloc(), frame.readableBytes(), frame)));
+    }
+
+    @Override
+    public Mono<Void> onClose() {
+        return onClose.asMono();
+    }
+
+    @Override
+    public void dispose() {
+        connection.dispose();
+        onClose.tryEmitEmpty();
     }
 }
